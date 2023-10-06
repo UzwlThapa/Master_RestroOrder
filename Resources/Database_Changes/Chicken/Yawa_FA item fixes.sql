5db069a096e3bf76c4de077bc0efac8310634415
iamtheak <karki.adarsha2005@gmail.com>
2023-10-06 11:02:02 +0545

    money round issue

--- Resources/Database_Changes/Chicken/Yawa_FA item fixes.sql
+++ Resources/Database_Changes/Chicken/Yawa_FA item fixes.sql
@@ -43,18 +43,7 @@ end
  
 DECLARE @RegularPurchaseId int 
 SELECT @RegularPurchaseId  = FinancialAcID from Ac_FinancialAc where Name = 'REGULAR PURCHASE A/C'
-  
---- add SALES DISCOUNT A/C
-if not exists(select 1 from Ac_FinancialAc where name = 'SALES DISCOUNT A/C')
-begin
-INSERT INTO Ac_FinancialAc(Name,PFinancialAcID,FinancialSysID,AddedBy,AddedOn,IsArchived,IsDebit,AccEntryType) 
-VALUES('SALES DISCOUNT A/C',@RegularPurchaseId,@TransactionNodeId,'danfe',GETDATE(),0,1,@AccEntryTypeId);
-end
-else
-begin
-UPDATE Ac_FinancialAc SET FinancialSysID = @TransactionNodeId,PFinancialAcID = @RegularPurchaseId,IsDebit = 1,AccEntryType = @AccEntryTypeId WHERE name ='SALES DISCOUNT A/C'
-end
-
+   
 --- add Jaar water
 if not exists(select 1 from Ac_FinancialAc where name = 'JAAR WATER')
 begin
@@ -149,6 +138,79 @@ UPDATE Ac_FinancialAc SET FinancialSysID = @TransactionNodeId,PFinancialAcID = @
 end
 
 
+ /* 
+	 -- EXPENSES 
+	 -- SALARY EXPENSES
+ */
+
+DECLARE @ExpensesId int
+SELECT @ExpensesId  = FinancialAcID from Ac_FinancialAc where Name = 'EXPENSES'
+
+--- add DIRECT EXPENSES
+if not exists(select 1 from Ac_FinancialAc where name = 'SALARY EXPENSES')
+begin
+INSERT INTO Ac_FinancialAc(Name,PFinancialAcID,FinancialSysID,AddedBy,AddedOn,IsArchived,IsDebit,AccEntryType) 
+VALUES('SALARY EXPENSES',@ExpensesId,@GroupNodeId,'danfe',GETDATE(),0,1,@AccEntryTypeId);
+end
+else
+begin
+UPDATE Ac_FinancialAc SET FinancialSysID = @GroupNodeId,PFinancialAcID = @ExpensesId,IsDebit = 1,AccEntryType = @AccEntryTypeId WHERE name ='SALARY EXPENSES'
+end
+ 
+DECLARE @SalaryExpensesId int 
+SELECT @SalaryExpensesId  = FinancialAcID from Ac_FinancialAc where Name = 'SALARY EXPENSES'
+
+ --- add Salary A/C
+if not exists(select 1 from Ac_FinancialAc where name = 'Salary A/C')
+begin
+INSERT INTO Ac_FinancialAc(Name,PFinancialAcID,FinancialSysID,AddedBy,AddedOn,IsArchived,IsDebit,AccEntryType) 
+VALUES('Salary A/C',@SalaryExpensesId,@TransactionNodeId,'danfe',GETDATE(),0,1,@AccEntryTypeId);
+end
+else
+begin
+UPDATE Ac_FinancialAc SET FinancialSysID = @TransactionNodeId,PFinancialAcID = @SalaryExpensesId,IsDebit = 1,AccEntryType = @AccEntryTypeId WHERE name ='Salary A/C'
+end
+ 
+
+ 
+--- add TOTAL DISCOUNT
+if not exists(select 1 from Ac_FinancialAc where name = 'TOTAL DISCOUNT')
+begin
+INSERT INTO Ac_FinancialAc(Name,PFinancialAcID,FinancialSysID,AddedBy,AddedOn,IsArchived,IsDebit,AccEntryType) 
+VALUES('TOTAL DISCOUNT',@ExpensesId,@GroupNodeId,'danfe',GETDATE(),0,1,@AccEntryTypeId);
+end
+else
+begin
+UPDATE Ac_FinancialAc SET FinancialSysID = @GroupNodeId,PFinancialAcID = @ExpensesId,IsDebit = 1,AccEntryType = @AccEntryTypeId WHERE name ='TOTAL DISCOUNT'
+end
+ 
+DECLARE @TotalDiscountId int 
+SELECT @TotalDiscountId  = FinancialAcID from Ac_FinancialAc where Name = 'TOTAL DISCOUNT'
+
+ --- add SALES DISCOUNT A/C
+if not exists(select 1 from Ac_FinancialAc where name = 'SALES DISCOUNT A/C')
+begin
+INSERT INTO Ac_FinancialAc(Name,PFinancialAcID,FinancialSysID,AddedBy,AddedOn,IsArchived,IsDebit,AccEntryType) 
+VALUES('SALES DISCOUNT A/C',@TotalDiscountId,@TransactionNodeId,'danfe',GETDATE(),0,1,@AccEntryTypeId);
+end
+else
+begin
+UPDATE Ac_FinancialAc SET FinancialSysID = @TransactionNodeId,PFinancialAcID = @TotalDiscountId,IsDebit = 1,AccEntryType = @AccEntryTypeId WHERE name ='SALES DISCOUNT A/C'
+end
+ 
+ 
+ --- add SURPLUS/DEFLICT A/C
+if not exists(select 1 from Ac_FinancialAc where name = 'SURPLUS/DEFLICT A/C')
+begin
+INSERT INTO Ac_FinancialAc(Name,PFinancialAcID,FinancialSysID,AddedBy,AddedOn,IsArchived,IsDebit,AccEntryType) 
+VALUES('SURPLUS/DEFLICT A/C',@TotalDiscountId,@TransactionNodeId,'danfe',GETDATE(),0,1,@AccEntryTypeId);
+end
+else
+begin
+UPDATE Ac_FinancialAc SET FinancialSysID = @TransactionNodeId,PFinancialAcID = @TotalDiscountId,IsDebit = 1,AccEntryType = @AccEntryTypeId WHERE name ='SURPLUS/DEFLICT A/C'
+end
+ 
+
 
 /* 
 	 -- INCOME  
@@ -214,17 +276,6 @@ end
 else
 begin
 UPDATE Ac_FinancialAc SET FinancialSysID = @TransactionNodeId,PFinancialAcID = @salesAcId,IsDebit = 0,AccEntryType = @AccEntryTypeId WHERE name ='PURCHASE DISCOUNT A/C'
-end
-
---- add SURPLUS/DEFLICT A/C
-if not exists(select 1 from Ac_FinancialAc where name = 'SURPLUS/DEFLICT A/C')
-begin
-INSERT INTO Ac_FinancialAc(Name,PFinancialAcID,FinancialSysID,AddedBy,AddedOn,IsArchived,IsDebit,AccEntryType) 
-VALUES('SURPLUS/DEFLICT A/C',@salesAcId,@TransactionNodeId,'danfe',GETDATE(),0,0,@AccEntryTypeId);
-end
-else
-begin
-UPDATE Ac_FinancialAc SET FinancialSysID = @TransactionNodeId,PFinancialAcID = @salesAcId,IsDebit = 0,AccEntryType = @AccEntryTypeId WHERE name ='SURPLUS/DEFLICT A/C'
 end
  
    
--- SageFrame/Scripts/main.js
+++ SageFrame/Scripts/main.js
@@ -1,3 +1,8 @@
 ﻿function moneyPlaceholder(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	if(x != null){ 
+		x = parseFloat(x).toFixed(2);
+		return x.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	} else {
+		return 0;
+	}
 }
