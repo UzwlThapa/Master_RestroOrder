38477d918232c86f4fe86460083a345031380a86
Yawahang Rai <yawahangkoyu@gmail.com>
2024-04-09 18:51:27 +0545

    sss

--- SageFrame/js/BillBind.js
+++ SageFrame/js/BillBind.js
@@ -204,7 +204,7 @@ function getBill(salesMasterId, foodCourtOrder) {
                 var dateSegment = billBody[0].Date.split(' ');
                 var timeSegment = dateSegment[1].split(':');
                 var time = `${timeSegment[0]}:${timeSegment[1]}`;
-                htmls += "<tr><td colspan='2' style='text-align:left;font-size:11px;'>Invoice Date : " + dateSegment[0] + " " + time + " " + dateSegment[2] + "</td>";
+                htmls += "<tr><td colspan='2' style='text-align:left;font-size:11px;'>Invoice Date : " + dateSegment[0] + "</td><td colspan='2' style='text-align:right;font-size:11px;'>Time : " + time + dateSegment[2] + "</td>";
 
                 htmls += "</tr>";
             }
